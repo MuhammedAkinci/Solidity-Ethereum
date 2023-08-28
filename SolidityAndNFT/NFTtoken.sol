6149377e1636c8de0282cf3ecf66402c9535d652
Muhammed Akıncı <95518574+MuhammedAkinci@users.noreply.github.com>
2023-08-28 19:09:36 +0300

    Update NFTtoken.sol

--- SolidityAndNFT/NFTtoken.sol
+++ SolidityAndNFT/NFTtoken.sol
@@ -5,62 +5,62 @@ import "@openzeppelin/contracts/token/ERC1155/ERC1155.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
 contract NFTToken is ERC1155, Ownable {
-    // ID'ler eğitim türlerini temsil eder
+    // IDs represent types of education
     uint256 public constant COURSE = 0;
     uint256 public constant WORKSHOP = 1;
 
-    // Eğitim türleri için adresler ve bilgiler
+    // Addresses and information for education types
     mapping(uint256 => mapping(bytes32 => bool)) public students;
     mapping(uint256 => mapping(address => address)) public instructorsAddresses;
     mapping(uint256 => mapping(address => string)) public courseInfo;
 
-    // Eğitmenlerin hesapları
+    // Instructor accounts
     mapping(address => uint256) public instructorBalances;
 
-    // Eğitim ücret oranı (0-100 arasında)
-    uint256 public commissionRate = 10; // %10 komisyon oranı
+    // Education fee rate (between 0 and 100)
+    uint256 public commissionRate = 10; // 10% commission rate
 
     constructor() ERC1155("https://your-api-url/{id}.json") {
-        // Contract sahibi, Ownable'ın sahibi olarak atanır
+        // The contract owner is set as the owner of Ownable
         transferOwnership(msg.sender);
     }
 
-    // Yeni bir eğitim oluşturur
+    // Creates a new education
     function createCourse(uint256 _id, address _instructor, address _student, string memory _courseInfo) public onlyOwner {
         require(_instructor != address(0), "Invalid instructor address");
         require(_student != address(0), "Invalid student address");
 
-        bytes32 studentKey = keccak256(abi.encodePacked(_student)); // Öğrenci adresinden benzersiz bir anahtar oluştur
+        bytes32 studentKey = keccak256(abi.encodePacked(_student)); // Create a unique key from student address
 
         instructorsAddresses[_id][_instructor] = _instructor;
-        students[_id][studentKey] = true; // Öğrenci kayıtlı olduğunda true olarak ayarla
+        students[_id][studentKey] = true; // Set to true when a student is enrolled
         courseInfo[_id][_student] = _courseInfo;
 
         _mint(_instructor, _id, 1, "");
         _mint(_student, _id, 1, "");
     }
 
-    // Eğitim bilgisini günceller
+    // Updates education information
     function updateCourseInfo(uint256 _id, string memory _newCourseInfo) public {
         address student = msg.sender;
-        bytes32 studentKey = keccak256(abi.encodePacked(student)); // Öğrenci adresinden anahtar oluştur
-        require(students[_id][studentKey], "Not enrolled in the course"); // Kontrol değerini boolean ile kontrol et
+        bytes32 studentKey = keccak256(abi.encodePacked(student)); // Create a key from student address
+        require(students[_id][studentKey], "Not enrolled in the course"); // Check the boolean value using control
 
         courseInfo[_id][student] = _newCourseInfo;
     }
 
-    // Eğitim ücretini hesaplar
+    // Calculates the commission fee for education
     function calculateCommission(uint256 _amount) internal view returns (uint256) {
         return (_amount * commissionRate) / 100;
     }
 
-    // Eğitmenin hesabına komisyonu gönderir
+    // Sends commission fee to instructor's account
     function sendCommission(address _instructor, uint256 _amount) internal {
         uint256 commissionAmount = calculateCommission(_amount);
         instructorBalances[_instructor] += commissionAmount;
     }
 
-    // Eğitmenin hesabındaki komisyonu çeker
+    // Withdraws commission from instructor's account
     function withdrawCommission() public {
         address instructor = msg.sender;
         uint256 balance = instructorBalances[instructor];
@@ -70,12 +70,12 @@ contract NFTToken is ERC1155, Ownable {
         payable(instructor).transfer(balance);
     }
 
-    // ERC1155 standardının fonksiyonlarını devralır
+    // Inherits functions from ERC1155 standard
     function uri(uint256 _id) public view override returns (string memory) {
         return string(abi.encodePacked(super.uri(_id), uint2str(_id)));
     }
 
-    // Heler fonksiyon, uint256'ı string'e çevirir
+    // Helper function to convert uint256 to string
     function uint2str(uint256 _i) internal pure returns (string memory _uintAsString) {
         if (_i == 0) {
             return "0";
