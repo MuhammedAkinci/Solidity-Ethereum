657ee91c3fe065fdf860a40e85fbaa6b27555eb4
Muhammed Akıncı <95518574+MuhammedAkinci@users.noreply.github.com>
2023-08-28 19:10:28 +0300

    Update burnStakeContract.sol

--- SolidityAndNFT/burnStakeContract.sol
+++ SolidityAndNFT/burnStakeContract.sol
@@ -4,23 +4,23 @@ pragma solidity ^0.8.1;
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 
-// BurnStake kontratı
+// BurnStake contract
 contract BurnStake is Ownable {
-    IERC20 public rewardToken; // Ödül tokenı
-    address public nftContractAddress; // NFTToken kontratının adresi
+    IERC20 public rewardToken; // Reward token
+    address public nftContractAddress; // Address of the NFTToken contract
 
     constructor(address _rewardToken, address _nftContractAddress) {
-        rewardToken = IERC20(_rewardToken); // Ödül tokenının adresini belirle
-        nftContractAddress = _nftContractAddress; // NFTToken kontratının adresini belirle
+        rewardToken = IERC20(_rewardToken); // Set the address of the reward token
+        nftContractAddress = _nftContractAddress; // Set the address of the NFTToken contract
     }
 
     function burnNFT(uint256 _id, address _student) external onlyOwner {
-        INFTToken nftContract = INFTToken(nftContractAddress); // NFTToken kontratına erişim için arayüzü kullan
-        nftContract.burnNFT(_id, _student); // NFT'yi yakma fonksiyonunu çağır
+        INFTToken nftContract = INFTToken(nftContractAddress); // Use the interface to access the NFTToken contract
+        nftContract.burnNFT(_id, _student); // Call the burn NFT function
     }
 }
 
-    // NFTToken kontratı için oluşturulan arayüz
-    interface INFTToken {
-        function burnNFT(uint256 _id, address _student) external; // NFT'yi yakma fonksiyonunu tanımla
-    }
+// Interface created for the NFTToken contract
+interface INFTToken {
+    function burnNFT(uint256 _id, address _student) external; // Define the burn NFT function
+}
