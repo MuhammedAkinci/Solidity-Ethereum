02536fca72f73496d5c5c5c6d811634e2fce9cfd
Muhammed Akıncı <95518574+MuhammedAkinci@users.noreply.github.com>
2023-08-28 19:11:21 +0300

    Update stakeContract.sol

--- SolidityAndNFT/stakeContract.sol
+++ SolidityAndNFT/stakeContract.sol
@@ -1,69 +1,69 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.1;
 
-// IERC20 token interfaceini kullanmak için gerekli kütüphane
+// Library needed to use IERC20 token interface
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
-// Sadece sahibin çağırabileceği fonksiyonları içeren Ownable kontratı
+// Ownable contract containing functions callable only by the owner
 import "@openzeppelin/contracts/access/Ownable.sol";
 
-// NFT staking kontratı
+// NFT staking contract
 contract NFTStaking is Ownable {
-    // Ödül tokenı
+    // Reward token
     IERC20 public rewardToken;
-    // Saniye başına ödül miktarı
+    // Reward amount per second
     uint256 public rewardPerSecond;
-    // Staking süresi (saniye cinsinden)
+    // Staking duration (in seconds)
     uint256 public stakingDuration;
     
-    // Kullanıcı staking miktarları
+    // User staking amounts
     mapping(address => uint256) public stakes;
-    // Staking başlangıç zamanları
+    // Staking start times
     mapping(address => uint256) public startTimes;
-    // Kullanıcı eğitime kayıtlı mı
+    // Is the user enrolled in the program
     mapping(address => bool) public isEnrolled;
 
-    // Kontrat oluşturulurken ödül tokenı, ödül miktarı ve staking süresi atanır
+    // When creating the contract, reward token, reward amount per second, and staking duration are assigned
     constructor(address _rewardToken, uint256 _rewardPerSecond, uint256 _stakingDuration) {
-        // IERC20'ye dönüşüm yaparak ödül tokenı atanır
+        // Assign the reward token by converting to IERC20
         rewardToken = IERC20(_rewardToken);
         rewardPerSecond = _rewardPerSecond;
         stakingDuration = _stakingDuration;
     }
 
-    // Kullanıcıyı eğitime kaydolmuş olarak işaretler
+    // Marks the user as enrolled in the program
     function enroll() external {
         require(!isEnrolled[msg.sender], "Already enrolled");
         isEnrolled[msg.sender] = true;
     }
 
-    // Staking yapma fonksiyonu
+    // Function for staking
     function stake(uint256 _amount) external {
-        require(isEnrolled[msg.sender], "Not enrolled"); // Kullanıcının eğitime kayıtlı olup olmadığını kontrol eder
+        require(isEnrolled[msg.sender], "Not enrolled"); // Checks if the user is enrolled
         require(_amount > 0, "Amount must be greater than 0");
         require(stakes[msg.sender] == 0, "Already staked");
 
-        // Kullanıcıdan ödül tokenlarını alır ve staking miktarını kaydeder
+        // Takes reward tokens from the user and records the staking amount
         rewardToken.transferFrom(msg.sender, address(this), _amount);
         stakes[msg.sender] = _amount;
         startTimes[msg.sender] = block.timestamp;
     }
 
-    // Stakingi sonlandırma fonksiyonu
+    // Function for unstaking
     function unstake() external {
         require(isEnrolled[msg.sender], "Not enrolled");
         require(stakes[msg.sender] > 0, "No staked amount");
         require(block.timestamp >= startTimes[msg.sender] + stakingDuration, "Staking duration not passed");
 
-        // Ödülü hesaplar ve kullanıcıya ödülü ve staking miktarını verir
+        // Calculates the reward and gives the reward and staked amount to the user
         uint256 reward = calculateReward(msg.sender);
         rewardToken.transfer(msg.sender, stakes[msg.sender] + reward);
 
-        // Kullanıcının staking ve başlangıç zamanlarını sıfırlar
+        // Resets user's staking and start times
         stakes[msg.sender] = 0;
         startTimes[msg.sender] = 0;
     }
 
-    // Kullanıcının alacağı ödülü hesaplar
+    // Calculates the reward for the user
     function calculateReward(address _user) public view returns (uint256) {
         if (block.timestamp < startTimes[_user] + stakingDuration) {
             uint256 stakedTime = block.timestamp - startTimes[_user];
@@ -72,17 +72,17 @@ contract NFTStaking is Ownable {
         return (stakingDuration * rewardPerSecond) * stakes[_user];
     }
 
-    // Sadece sahibin çağırabileceği ödül miktarını güncelleme fonksiyonu
+    // Function to update reward amount per second (callable only by owner)
     function updateRewardPerSecond(uint256 _newRewardPerSecond) external onlyOwner {
         rewardPerSecond = _newRewardPerSecond;
     }
 
-    // Sadece sahibin çağırabileceği staking süresini güncelleme fonksiyonu
+    // Function to update staking duration (callable only by owner)
     function updateStakingDuration(uint256 _newStakingDuration) external onlyOwner {
         stakingDuration = _newStakingDuration;
     }
 
-    // Sadece sahibin çağırabileceği ödül tokenlarını çekme fonksiyonu
+    // Function to withdraw reward tokens (callable only by owner)
     function withdrawRewardTokens(uint256 _amount) external onlyOwner {
         rewardToken.transfer(owner(), _amount);
     }
